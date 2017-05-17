f29737f87e5bd640f7a266e14e4b280ede7d5766
misha <misha@>
2017-05-17 10:48:49 +0300

    Pomulka metod Div CalcClass

--- CalcClass/MathOperations.cs
+++ CalcClass/MathOperations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -67,7 +67,16 @@ namespace CalcClass
             {
                 result = (int)(a / b);
             }
-            // тут вставляєм 4
+            catch (OverflowException) 
+{ 
+_lastError = "Error 06"; 
+result = 0; 
+} 
+catch (DivideByZeroException) 
+{ 
+_lastError = "Error 09"; 
+result = 0; 
+} 
             return result;
         }
         public static int Mod(long a, long b)
@@ -92,7 +101,7 @@ namespace CalcClass
         public static int ABS(long a)
         {
             int result;
-            // тут 5.
+            // ��� 5.
         }
         public static int IABS(long a)
         {
